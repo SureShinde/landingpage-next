40db244a5e27277e0871dde2d82594b4617f3750
aryawirasandi <aryawirasandi@github.com>
2020-11-11 18:01:30 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import main from "../styles/index.module.scss"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container, main.background}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
